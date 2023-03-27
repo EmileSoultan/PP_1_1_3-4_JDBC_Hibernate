9775fb118e6c7408b8c620667c754b64c60611b6
Emile <emabish42@gmail.com>
2023-03-27 22:16:59 +0200

    Реализация алгоритма, согласно  заданию

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,20 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+
+
 public class Main {
     public static void main(String[] args) {
-        // СЂРµР°Р»РёР·СѓР№С‚Рµ Р°Р»РіРѕСЂРёС‚Рј Р·РґРµСЃСЊ
+        // реализуйте алгоритм здесь
+        UserDao userDao = UserDaoJDBCImpl.getInstance();
+        userDao.createUsersTable();
+        userDao.saveUser("Федор", "Иванов", (byte) 32);
+        userDao.saveUser("Евгений", "Смирнов", (byte) 42);
+        userDao.saveUser("Елена", "Котова", (byte) 22);
+        userDao.saveUser("Ирина", "Байкова", (byte) 25);
+        userDao.getAllUsers();
+        userDao.cleanUsersTable();
+        userDao.dropUsersTable();
     }
 }
