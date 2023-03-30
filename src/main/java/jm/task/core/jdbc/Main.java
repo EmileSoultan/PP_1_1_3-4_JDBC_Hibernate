ad8bab160b1cf16b85283d7d060076a318fe82e0
Emile <emabish42@gmail.com>
2023-03-30 09:58:20 +0200

    Небольшие правки

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -3,9 +3,6 @@ package jm.task.core.jdbc;
 import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 
-import java.util.HashMap;
-import java.util.Map;
-
 
 public class Main {
     public static void main(String[] args) {
